1e5287ddeff870e33867553229183b0832893cb3
joeyalod23 <joeyalod23@users.noreply.github.com>
2026-07-23 15:49:03 +0800

    Add deployment config for Render hosting

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+php artisan key:generate --force
+php artisan migrate --force
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan storage:link
+php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
